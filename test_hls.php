1d31ab9d5b219fc674ce1402cb2644d1ece8c8d7
helloabob <wangbo@wangbos-iMac.local>
2014-07-11 07:34:15 +0800

    update

---
+++ test_hls.php
@@ -0,0 +1,14 @@
+
+<center>
+<div>
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" name="player1" id="player1" width="698" height="433"><param name="movie" value="TestHLSVideo.swf?t=<?php echo time() ?>">
+<param name="flashvars" value="playerId=84863534562803713&amp;apiDomain=apivideo.kankanews.com">
+<param name="allowFullScreen" value="true">
+<param name="wmode" value="transparent">
+<param name="allowscriptaccess" value="always">
+<embed src="TestHLSVideo.swf?t=<?php echo time() ?>" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" wmode="opaque" width="698" height="433" name="player2" id="player2" flashvars="playerId=84863534562803713&amp;apiDomain=apivideo.kankanews.com">
+</object>
+</div>
+</center>
+
+
