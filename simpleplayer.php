94238ab37598e8666940611831e334ff88ce82e2
helloabob <wangbo@wangbos-iMac.local>
2014-07-31 23:39:13 +0800

    update

---
+++ simpleplayer.php
@@ -0,0 +1,15 @@
+
+<center>
+<div>
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" name="player1" id="player1" width="960" height="528">
+<param name="movie" value="simpleplayer.swf?t=<?php echo time() ?>">
+<param name="flashvars" value="url=http://lmsvideo.smgtech.net/lms/1/Live.m3u8">
+<param name="allowFullScreen" value="true">
+<param name="wmode" value="direct">
+<param name="allowscriptaccess" value="always">
+<embed src="simpleplayer.swf?t=<?php echo time() ?>" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" wmode="direct" width="960" height="528" name="player2" id="player2" flashvars="url=http://lmsvideo.smgtech.net/lms/1/Live.m3u8">
+</object>
+</div>
+</center>
+
+
